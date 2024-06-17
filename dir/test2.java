c1a3ddd7a6fd05d6119b1760ebe52307443a5579
PARK GI-PYO <103515256+pyo92@users.noreply.github.com>
2024-06-17 21:54:26 +0900

    Add test.java with Hello World content

--- dir/test2.java
+++ dir/test2.java
@@ -1,1 +1,1 @@
-public class test { public static void main(String[] args) { System.out.println("Hello World!"); } }
+public class test22 { public static void main(String[] args) { System.out.println("Hello World!"); } }
