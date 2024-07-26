9090cd0bef8379b2af0280a81baf1e8333a1342c
PARK GI-PYO <103515256+pyo92@users.noreply.github.com>
2024-07-26 21:47:08 +0900

    Solved source code is uploaded.

---
+++ boj/[010989] 수 정렬하기 3/Main.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static BufferedWriter bw;
+
+    static StringBuilder out = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            out.append(arr[i]);
+            out.append('\n');
+        }
+
+        bw.write(out.toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
